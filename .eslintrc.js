4548ae29862456efcf7b44ab0d51c1d1c742ed43
Cristina Tarantino <cristina@tarantino.io>
2020-08-28 15:32:24 +0100

    Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
     "prettier",
     "prettier/react",
     "prettier/@typescript-eslint",
+    "airbnb-typescript-prettier",
     "plugin:prettier/recommended",
   ],
   plugins: ["react", "@typescript-eslint", "jest"],

f2e1e2e8957579b0f66680da4efc948792b4fd8f
Cristina Tarantino <cristina@tarantino.io>
2020-09-03 16:15:08 +0100

    Updates

---
+++ precache-manifest.bc6685f56c96890d31c05034aeab0f2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cd518c1676ea95a723bb78981c582a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b4ffd651c691f78fc4f",
+    "url": "/static/css/main.5f92c3bc.chunk.css"
+  },
+  {
+    "revision": "014e8b067ef3f1eb906e",
+    "url": "/static/js/2.c4839891.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4839891.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b4ffd651c691f78fc4f",
+    "url": "/static/js/main.6312e48b.chunk.js"
+  },
+  {
+    "revision": "b124995adca1e76c588b",
+    "url": "/static/js/runtime-main.3712b1dd.js"
+  }
+]);
